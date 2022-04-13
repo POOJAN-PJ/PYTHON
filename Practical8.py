ae25fdba5ee39ef09f76a2764b2d80a344489737
POOJAN PATEL <103519322+POOJAN-PJ@users.noreply.github.com>
2022-04-13 14:01:53 +0530

    Create Practical8.py

---
+++ Practical8.py
@@ -0,0 +1,32 @@
+class Stack:
+
+    def _init_(self):
+        self.items = []
+        top = None
+
+    def is_empty(self):
+        return self.items == []
+
+    # push function
+    def push(self, data):
+        self.items.append(data)
+
+    # pop function
+    def pop(self):
+        return self.items.pop()
+    
+    def traversal(self):
+        for i in self.items:
+            print(i)
+
+s = Stack()
+
+s.push(1)
+s.push(2)
+s.push(3)
+print("Elements in Stack : ")
+s.traversal()
+print("Pop element : ",s.pop())
+print("Pop element : ",s.pop())
+print("After pop Elements in Stack : ")
+s.traversal()
